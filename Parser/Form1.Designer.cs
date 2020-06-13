1b15eddda13f579455e3d348b60bfc7d1504676a
Nevajno-kto <54955074+Nevajno-kto@users.noreply.github.com>
2020-06-13 13:13:44 +0300

    Add files via upload

---
+++ Parser/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Parser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Path = new Guna.UI2.WinForms.Guna2TextBox();
+            this.Url = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.guna2CircleProgressBar1 = new Guna.UI2.WinForms.Guna2CircleProgressBar();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.Result = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.log = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Uri сайта";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Путь для сохранения";
+            // 
+            // Path
+            // 
+            this.Path.BorderRadius = 10;
+            this.Path.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.Path.DefaultText = "";
+            this.Path.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.Path.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.Path.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.Path.DisabledState.Parent = this.Path;
+            this.Path.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.Path.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Path.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.Path.FocusedState.Parent = this.Path;
+            this.Path.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.Path.HoverState.Parent = this.Path;
+            this.Path.Location = new System.Drawing.Point(12, 66);
+            this.Path.Name = "Path";
+            this.Path.PasswordChar = '\0';
+            this.Path.PlaceholderText = "";
+            this.Path.SelectedText = "";
+            this.Path.ShadowDecoration.Parent = this.Path;
+            this.Path.Size = new System.Drawing.Size(173, 23);
+            this.Path.TabIndex = 6;
+            this.Path.DoubleClick += new System.EventHandler(this.Path_DoubleClick);
+            // 
+            // Url
+            // 
+            this.Url.BorderRadius = 10;
+            this.Url.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.Url.DefaultText = "";
+            this.Url.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.Url.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.Url.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.Url.DisabledState.Parent = this.Url;
+            this.Url.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.Url.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Url.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.Url.FocusedState.Parent = this.Url;
+            this.Url.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.Url.HoverState.Parent = this.Url;
+            this.Url.Location = new System.Drawing.Point(12, 25);
+            this.Url.Name = "Url";
+            this.Url.PasswordChar = '\0';
+            this.Url.PlaceholderText = "";
+            this.Url.SelectedText = "";
+            this.Url.ShadowDecoration.Parent = this.Url;
+            this.Url.Size = new System.Drawing.Size(173, 22);
+            this.Url.TabIndex = 7;
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.BorderRadius = 12;
+            this.guna2Button1.CheckedState.Parent = this.guna2Button1;
+            this.guna2Button1.CustomImages.Parent = this.guna2Button1;
+            this.guna2Button1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button1.ForeColor = System.Drawing.Color.Black;
+            this.guna2Button1.HoverState.Parent = this.guna2Button1;
+            this.guna2Button1.Location = new System.Drawing.Point(456, 2);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.ShadowDecoration.Parent = this.guna2Button1;
+            this.guna2Button1.Size = new System.Drawing.Size(25, 25);
+            this.guna2Button1.TabIndex = 9;
+            this.guna2Button1.Text = "X";
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // guna2Button2
+            // 
+            this.guna2Button2.BorderColor = System.Drawing.Color.Transparent;
+            this.guna2Button2.BorderRadius = 15;
+            this.guna2Button2.CheckedState.Parent = this.guna2Button2;
+            this.guna2Button2.CustomImages.Parent = this.guna2Button2;
+            this.guna2Button2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.guna2Button2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button2.ForeColor = System.Drawing.Color.Black;
+            this.guna2Button2.HoverState.Parent = this.guna2Button2;
+            this.guna2Button2.Location = new System.Drawing.Point(25, 104);
+            this.guna2Button2.Name = "guna2Button2";
+            this.guna2Button2.ShadowDecoration.Parent = this.guna2Button2;
+            this.guna2Button2.Size = new System.Drawing.Size(141, 27);
+            this.guna2Button2.TabIndex = 10;
+            this.guna2Button2.Text = "Вывести страницы";
+            this.guna2Button2.Click += new System.EventHandler(this.guna2Button2_Click);
+            // 
+            // guna2Elipse1
+            // 
+            this.guna2Elipse1.BorderRadius = 10;
+            this.guna2Elipse1.TargetControl = this;
+            // 
+            // guna2CircleProgressBar1
+            // 
+            this.guna2CircleProgressBar1.Animated = true;
+            this.guna2CircleProgressBar1.FillColor = System.Drawing.SystemColors.Control;
+            this.guna2CircleProgressBar1.FillThickness = 20;
+            this.guna2CircleProgressBar1.Location = new System.Drawing.Point(25, 162);
+            this.guna2CircleProgressBar1.Name = "guna2CircleProgressBar1";
+            this.guna2CircleProgressBar1.ProgressColor = System.Drawing.Color.Black;
+            this.guna2CircleProgressBar1.ProgressColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.guna2CircleProgressBar1.ProgressThickness = 20;
+            this.guna2CircleProgressBar1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.guna2CircleProgressBar1.ShadowDecoration.Parent = this.guna2CircleProgressBar1;
+            this.guna2CircleProgressBar1.ShowPercentage = true;
+            this.guna2CircleProgressBar1.Size = new System.Drawing.Size(130, 130);
+            this.guna2CircleProgressBar1.TabIndex = 11;
+            // 
+            // Result
+            // 
+            this.Result.AutoSize = true;
+            this.Result.Location = new System.Drawing.Point(287, 25);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(0, 13);
+            this.Result.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 315);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Лог программы :";
+            // 
+            // log
+            // 
+            this.log.AutoSize = true;
+            this.log.Location = new System.Drawing.Point(113, 315);
+            this.log.Name = "log";
+            this.log.Size = new System.Drawing.Size(0, 13);
+            this.log.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(487, 393);
+            this.Controls.Add(this.log);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.guna2CircleProgressBar1);
+            this.Controls.Add(this.guna2Button2);
+            this.Controls.Add(this.guna2Button1);
+            this.Controls.Add(this.Url);
+            this.Controls.Add(this.Path);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private Guna.UI2.WinForms.Guna2TextBox Path;
+        private Guna.UI2.WinForms.Guna2TextBox Url;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+        private Guna.UI2.WinForms.Guna2CircleProgressBar guna2CircleProgressBar1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label Result;
+        private System.Windows.Forms.Label log;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Parser/Form1.cs
@@ -0,0 +1,110 @@
+﻿using Parser.ParserDetails;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parser
+{
+    public partial class Form1 : Form
+    {
+        private List<SaveReferences> ListSR = new List<SaveReferences>();
+        private bool isWorking = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void MakesChoicePages(List<SaveReferences> pages)
+        {
+            CheckedListBox checkedLB = new CheckedListBox();
+            checkedLB.BorderStyle = BorderStyle.None;
+            checkedLB.Location = new System.Drawing.Point(215, 25);
+            checkedLB.BackColor = Color.FromKnownColor(KnownColor.Control);
+            checkedLB.Width = 200;
+            checkedLB.Height = 289;
+            for (int i = 0; i < pages.Count; i++)
+            {
+                CheckBox temp = new CheckBox();
+                temp.Name = pages[i].Name;
+                temp.Text = pages[i].Name;
+                checkedLB.Items.Add(temp);
+            }
+            checkedLB.DisplayMember = "Name";
+            checkedLB.ItemCheck += new ItemCheckEventHandler(this.ChangedCheckBox);
+            this.Controls.Add(checkedLB);
+        }
+
+        private void ChangedCheckBox(object sender, EventArgs e)
+        {
+            CheckedListBox checkedLb = (CheckedListBox)sender;
+            ListSR[checkedLb.SelectedIndex].IsDownload = !ListSR[checkedLb.SelectedIndex].IsDownload;
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            if (ParserHtml.ValidateUri(Url.Text))
+            {
+                log.Text = "";
+                Parser ViewPages = new Parser(Url.Text);
+                ListSR = ViewPages.StartGetPages();
+                MakesChoicePages(ListSR);
+                Guna.UI2.WinForms.Guna2Button parserB = new Guna.UI2.WinForms.Guna2Button();
+                parserB.BorderRadius = 15;
+                parserB.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+                parserB.Size = new System.Drawing.Size(141, 27);
+                parserB.Text = "Спарсить";
+                parserB.Location = new Point(239, 321);
+                parserB.Click += new System.EventHandler(this.Start);
+                parserB.ForeColor = Color.FromKnownColor(KnownColor.Black);
+                this.Controls.Add(parserB);
+            }
+            else
+            {
+                log.Text = "Не валидный Url";
+            }
+        }
+        /* http://www.css-tricks.ru/articles/css/attribute-selectors */
+        /*https://megabit32.ru/ */
+        private async void Start(object sender, EventArgs e)
+        {   
+            if (isWorking) { log.Text = "Программа запущена"; return; }
+            if (!ParserHtml.ValidateUri(Url.Text)) { log.Text = "Не валидный Url"; return; }
+            if(Path.Text == "") { log.Text = "Заполните поле :\n'Путь для сохранения' "; return; }
+            Parser main = new Parser(Url.Text, Path.Text, "index.html");
+            List<SaveReferences> UsefullList = new List<SaveReferences> (main.Preparation(ListSR));
+            if(UsefullList.Count == 0) { log.Text = "Не выбрана \nни одна страница"; return; }
+            log.Text = "";
+            isWorking = true;
+            log.Text = await Task.Run(() => main.MainPage(UsefullList, this));
+            isWorking = false;
+        }
+        public int ProgressValue
+        {
+            set { guna2CircleProgressBar1.Value = value; }
+            get { return guna2CircleProgressBar1.Value; }
+        }
+        public int ProgressMaxValue
+        {
+            set { guna2CircleProgressBar1.Maximum = value; }
+            get { return guna2CircleProgressBar1.Maximum; }
+        }
+        private void Path_DoubleClick(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                Path.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+    }
+}
